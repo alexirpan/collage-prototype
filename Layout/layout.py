e8c45d84a04e5863abb4238a49699374b06d6cfe
Alex Irpan <xelanapri@gmail.com>
2022-05-07 18:31:05 -0700

    Change to write + read the edge info from a separate JS file

--- Layout/layout.py
+++ Layout/layout.py
@@ -6,11 +6,15 @@ with open("Nodes.csv") as f:
 with open("Edges.csv") as f:
     edges  = list(csv.reader(f))[1:]
 
-for i, (name, ntype, fillno, ftype) in enumerate(nodes):
-    c = "orange" if ntype.startswith("target") and len(ntype) == 7 else "green"
-    print('{{ id: "{}", group: {}, label: "{}-{}", level: 1, color: "{}" }},'.format(name, i, name, ntype[-1], c))
-for u, v, etype in edges:
-    print('{{ target: "{}", source: "{}", strength: 0.1 }},'.format(v, u))
+with open("constants.js", "w") as f:
+    f.write('var nodes = [\n')
+    for i, (name, ntype, fillno, ftype) in enumerate(nodes):
+        c = "orange" if ntype.startswith("target") and len(ntype) == 7 else "green"
+        f.write('{{ id: "{}", group: {}, label: "{}-{}", level: 1, color: "{}" }},\n'.format(name, i, name, ntype[-1], c))
+    f.write(']\n\nvar links = [\n')
+    for u, v, etype in edges:
+        f.write('{{ target: "{}", source: "{}", strength: 0.1 }},\n'.format(v, u))
+    f.write(']\n')
 
 # nodes.forEach(function (n) { console.log(n.id, n.fx, n.fy)})
 # nodes.forEach(function (n) { console.log(n.id, Math.round(n.fx) - 1950, 2700 - Math.round(n.fy))})
