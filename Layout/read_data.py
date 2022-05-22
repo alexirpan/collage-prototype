007711ab839207d0ae00aaf06ddf953ac2aae3fe
Alex Irpan <xelanapri@gmail.com>
2022-05-21 17:26:54 -0700

    Move reading to a separate file, updated file names, copy to clipboard button for web

---
+++ Layout/read_data.py
@@ -0,0 +1,43 @@
+import csv
+# This only lists nodes that are special, by default nodes are added with the
+# default type
+with open("../Nodes.csv") as f:
+    nodes = list(csv.reader(f,delimiter=','))
+    # Drop header
+    nodes = nodes[1:]
+    nodes = [n for n in nodes if n]
+    # drop empty cells
+    nodes = [[t.strip() for t in row if t.strip()] for row in nodes]
+    # uppercase answers but not node type
+    for row in nodes:
+        row[0] = row[0].upper()
+
+# Adjacency list -> list of (u,v) edges
+edg = set()
+with open("../Edges.csv") as f:
+    rows = list(csv.reader(f,delimiter=','))
+    # Drop header
+    rows = rows[1:]
+    rows = [row for row in rows if row]
+    # drop empty cells
+    rows = [[t.upper().strip() for t in row if t.strip()] for row in rows]
+    for row in rows:
+        start = row[0]
+        for end in row[1:]:
+            edg.add(tuple(sorted([start, end])))
+
+
+# input validation and cleanup
+verts = set()
+for row in nodes:
+    verts.add(row[0])
+
+for u, v in edg:
+    if u not in verts:
+        nodes.append((u, 'regular'))
+        verts.add(u)
+    if v not in verts:
+        nodes.append((v, 'regular'))
+        verts.add(v)
+
+print('Read date')
--- Puzzle/locations.py
+++ Puzzle/locations.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 import csv
+import read_data
 
-# This only lists nodes that are special, by default nodes are added with the
-# default type
-with open("../Layout/Nodes.csv") as f:
-    nodes = list(csv.reader(f,delimiter=','))
-
-with open("../Layout/Edges.csv") as f:
-    edges = list(csv.reader(f,delimiter=','))
+nodes, edg, verts = read_data.nodes, read_data.edg, read_data.verts
 
 with open('positiondump') as f:
-    pos = f.readlines()
+    pos = f.read().split(',')
     pos = [line.strip() for line in pos if line.strip()]
     pos = [line.rsplit(' ', 2) for line in pos]
     positions = dict()
@@ -25,42 +20,23 @@ with open('positiondump') as f:
         x,y = positions[v]
         positions[v] = (x - tx, y - ty)
 
-# input validation and cleanup
-verts = []
-for row in nodes:
-    row[0] = row[0].upper()
-    verts.append(row[0])
-
-for row in edges:
-    u, v = row[0], row[1]
-    if len(row) == 2:
-        # default color
-        row.append('rgba(50, 50, 50, 0.5)')
-    row[0] = u.upper()
-    row[1] = v.upper()
-    if u.upper() not in verts:
-        nodes.append((u.upper(), 'regular'))
-        verts.append(u.upper())
-    if v.upper() not in verts:
-        nodes.append((v.upper(), 'regular'))
-        verts.append(v.upper())
-
 with open("locations.js", "w") as f:
     f.write('var nodes = [\n')
     # (name, type)
     for i, (name, ntype) in enumerate(nodes):
+        c = "green"
         if ntype.startswith("topic") or ntype.startswith("given"):
             c = "orange"
         elif ntype.startswith("hidden"):
             c = "red"
-        else:
-            c = "green"
+        elif ntype.startswith("answer"):
+            c = "pink"
 
         f.write('{{ id: "{}", group: {}, label: "{}", level: 1, color: "{}", x: "{}", y: "{}", ntype: "{}" }},\n'.format(name, i, name, c, positions[name][0], positions[name][1], ntype))
     f.write(']\n\nvar links = [\n')
     # All edges are undirected
     for u, v, color in edges:
-        f.write('{{ target: "{}", source: "{}", color: "{}", source_x: "{}", source_y: "{}", target_x: "{}", target_y: "{}" }},\n'.format(v, u, color, positions[u][0], positions[u][1], positions[v][0], positions[v][1]))
+        f.write('{{ target: "{}", source: "{}", color: "rgba(50,50,50,0.5)", source_x: "{}", source_y: "{}", target_x: "{}", target_y: "{}" }},\n'.format(v, u, positions[u][0], positions[u][1], positions[v][0], positions[v][1]))
     f.write(']\n')
 
 # nodes.forEach(function (n) { console.log(n.id, n.fx, n.fy)})
