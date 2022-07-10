bb7dbca7b8f104860516eb8f5585d4005e94f168
Alex Irpan <xelanapri@gmail.com>
2022-07-10 00:59:30 -0700

    Trying different experiments on graph layout

--- Layout/layout.py
+++ Layout/layout.py
@@ -4,26 +4,6 @@ import read_data
 
 nodes, edg, verts = read_data.nodes, read_data.edg, read_data.verts
 
-with open("constants.js", "w") as f:
-    f.write('var nodes = [\n')
-    # (name, type)
-    for i, row in enumerate(nodes):
-        name = row[0]
-        ntype = row[1]
-        c = "green"
-        if ntype.startswith("topic") or ntype.startswith("given"):
-            c = "orange"
-        elif ntype.startswith("hidden"):
-            c = "red"
-        elif ntype.startswith("answer"):
-            c = "pink"
-        f.write('{{ id: "{}", group: {}, label: "{}", level: 1, color: "{}" }},\n'.format(name, i, name, c))
-    f.write(']\n\nvar links = [\n')
-    # All edges are undirected
-    for u, v in edg:
-        f.write('{{ target: "{}", source: "{}", color: "rgba(50,50,50,0.5)", strength: 0.3 }},\n'.format(v, u))
-    f.write(']\n')
-
 # Report some vertex info
 degrees = {}
 for v in verts:
@@ -61,5 +41,45 @@ while to_visit:
 for v in verts:
     if v not in seen:
         print('  ' + v)
+
+# Increase strength based on number of neighboring edges - high density nodes should be
+# grouped more tightly
+# This didn't work so well.
+
+# Strength towards topics
+# Does okay but some high-degree nodes weird.
+
+# Strength towards topics or high degree nodes.
+
+central = set()
+
+with open("constants.js", "w") as f:
+    f.write('var nodes = [\n')
+    # (name, type)
+    for i, row in enumerate(nodes):
+        name = row[0]
+        ntype = row[1]
+        c = "green"
+        if ntype.startswith("topic") or ntype.startswith("given"):
+            c = "orange"
+            central.add(name)
+        elif ntype.startswith("hidden"):
+            c = "red"
+            central.add(name)
+        elif ntype.startswith("answer"):
+            c = "pink"
+            central.add(name)
+        f.write('{{ id: "{}", group: {}, label: "{}", level: 1, color: "{}" }},\n'.format(name, i, name, c))
+    f.write(']\n\nvar links = [\n')
+    # All edges are undirected
+    for u, v in edg:
+        #strength = 0.01 * degrees[u] * degrees[v]
+        strength = 0.1
+        if u in central or v in central:
+            strength = max(0.5, strength)
+        #if max(degrees[u], degrees[v]) >= 8:
+        #    strength = max(1.0, strength)
+        f.write('{{ target: "{}", source: "{}", color: "rgba(50,50,50,0.5)", strength: {} }},\n'.format(v, u, strength))
+    f.write(']\n')
 
 # nodes.forEach(function (n) { console.log(n.id, n.x, n.y)})
